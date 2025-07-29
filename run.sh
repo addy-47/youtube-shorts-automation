68df1999038f97eafe1340e6766b0241c741c9d3
addy-47 <adhbutg@gmail.com>
2025-07-29 10:37:49 +0530

    Automated commit before running script

--- run.sh
+++ run.sh
@@ -8,7 +8,6 @@ export PYTHONPATH="/home/addy/projects/youtube-shorts-automation"
 /usr/bin/git add .
 /usr/bin/git commit -m "Automated commit before running script"
 /usr/bin/git checkout master
-source /home/addy/.bashrc
-source /home/addy/projects/scripts/gcp-k8s/gcloud-kubectl-switch.sh && switch-addy
+gcloud config set account adhbutg@gmail.com
 /home/addy/projects/youtube-shorts-automation/.venv/bin/python /home/addy/projects/youtube-shorts-automation/main.py
 echo "[$(date)] Script execution completed." >> /home/addy/projects/youtube-shorts-automation/cron.log
